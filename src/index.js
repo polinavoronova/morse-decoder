7e637809a6ca72a0b41db8d7079a3c362b45a5fb
polina.voronova <polonium.pv@gmail.com>
2019-09-30 21:54:42 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,60 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const LETTER_LENGTH = 10;
+
 function decode(expr) {
-    // write your solution here
+  let splittedExpr = splitExpr(expr);
+  let result = [];
+  for (let elem of splittedExpr) {
+    let letter = decodeLetter(elem);
+    result.push(letter);
+  }
+  return result.join('');
+}
+
+function splitExpr(expr) {
+  let splittedExpr = [];
+  
+  let currentLetter = [];
+  for (let i = 0; i < expr.length; i++) {
+    currentLetter.push(expr[i])
+
+    if (currentLetter.length === LETTER_LENGTH) {
+      splittedExpr.push(currentLetter.join(''));
+      currentLetter = [];
+    }
+  }
+  
+  return splittedExpr;
+}
+
+function decodeLetter(encodedLetter) {
+  let pairs = {
+    '10' : '.',
+    '11' : '-'
+  };
+
+  if (encodedLetter[0] === '*') {
+    return ' ';
+  }
+
+  let letter = [];
+  let current = [];
+  for (let i = 0; i < LETTER_LENGTH; i ++) {
+    current.push(encodedLetter[i]);
+    if (current.length === 2) {
+      let char = pairs[current.join('')];
+      if (char) {
+        letter.push(char);
+      }
+      
+      current = [];
+    }
+  }
+
+  letter = letter.join('');
+  return MORSE_TABLE[letter];
 }
 
 module.exports = {
